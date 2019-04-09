b5065b1f1a334f73e8d11d5f1eefda746ad8608c
kiozak <aboudiazakaria@gmail.com>
2019-04-09 15:21:59 +0100

    better installation tutorial

--- README.md
+++
@@ -1,2 +0,0 @@
-# watch_dogs_v3_ubuntu_animation
-A modified Plymouth Boot Animation based on Watch dogs v3 animation (Youtube Video )
--- install.sh
+++
@@ -1,5 +0,0 @@
-sudo update-alternatives --install /usr/share/plymouth/themes/default.plymouth default.plymouth /usr/share/plymouth/themes/watch_dogs2/watch_dogs2.plymouth 100
-
-sudo update-alternatives --config default.plymouth
-
-sudo update-initramfs -u
